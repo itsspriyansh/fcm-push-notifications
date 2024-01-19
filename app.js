cdf2b82251d0690920a2014d9e57c30a75318e57
itsspriyansh <priyanshkp01@gmail.com>
2024-01-19 18:12:30 +0530

    removed dependency

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/send", function (req, res) {
     });
 });
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
