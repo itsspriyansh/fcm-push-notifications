23806117c78752eddb9517acc2340bc10f17ba32
itsspriyansh <priyanshkp01@gmail.com>
2024-02-28 06:56:49 +0530

    added send-image-notification route

--- app.js
+++ app.js
@@ -52,6 +52,49 @@ app.post("/send", function (req, res) {
     });
 });
 
+app.post("/send-image", function (req, res) {
+  const message = {
+    notification: {
+      title: req.body.title,
+    },
+    android: {
+      notification: {
+        imageUrl: req.body.image,
+      }
+    },
+    apns: {
+      payload: {
+        aps: {
+          'mutable-content': 1
+        }
+      },
+      fcm_options: {
+        image: req.body.image,
+      }
+    },
+    webpush: {
+      headers: {
+        image: req.body.image,
+      }
+    },
+    topic: "notify",
+  };
+  getMessaging()
+    .send(message)
+    .then((response) => {
+      res.status(200).json({
+        message: "successfully sent notification",
+        response: response,
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        message: "couldn't send notification",
+        error: error,
+      });
+    });
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
